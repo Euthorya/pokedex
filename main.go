899d3e60f60a59ac4e00e5428ffee9c6d27d1e5d
Mikhail Efimov <efimovhq@gmail.com>
2025-02-26 20:06:26 +0400

    Exit and help commands

--- main.go
+++ main.go
@@ -10,24 +10,37 @@ import (
 func main() {
 	//var reader io.Reader
 	scanner := bufio.NewScanner(os.Stdin)
-
+	commands := commands()
 	for {
 		fmt.Print("Pokedex >")
 		scanner.Scan()
 		input := scanner.Text()
 		words := cleanInput(input)
-		fmt.Printf("Your command was: %v\n", words[0])
+		data, err := commands[words[0]]
+		if !err {
+			fmt.Printf("Unknown command: %v\n", words[0])
+			continue
+		}
+		data.callback()
 	}
 
 }
 
 func cleanInput(text string) []string {
-	//fmt.Println(strings.Split(strings.Trim(strings.ToLower(text), " "), " "))
-	// var result []string
-	// for s := range strings.SplitSeq(strings.TrimSpace(strings.ToLower(text)), " ") {
-	// 	result = append(result, strings.TrimSpace(s))
-	// }
-	// return result
 	return strings.Fields(strings.Trim(strings.ToLower(text), " "))
 
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Printf("Welcome to the Pokedex!\nUsage:\n\n")
+	for _, c := range commands() {
+		fmt.Printf("%v: %v\n", c.name, c.description)
+	}
+	return nil
 }
